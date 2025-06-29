e00d5f25d05f77965a8685923de74e2a96dcff99
Basil Okache <basilokache@gmail.com>
2025-06-29 19:00:37 +0100

    Main go function added

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/ok-basil/bookstoremanagement/pkg/routes"
+	"log"
+	"net/http"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+}
